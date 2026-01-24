d13f0638a0533dc1d67f3a3e3f6164ca1006b95d
riccardo-fragale <ricfra32@gmail.com>
2026-01-24 18:19:54 +0100

    assignment 0-1

---
+++ dectrees/python/main.py
@@ -0,0 +1,13 @@
+import monkdata as m
+import dtree as dt
+
+def main():
+    print(dt.entropy(m.monk1))
+    print(dt.entropy(m.monk2))
+    print(dt.entropy(m.monk3))
+
+
+if __name__ == '__main__':
+    main()
+
+
