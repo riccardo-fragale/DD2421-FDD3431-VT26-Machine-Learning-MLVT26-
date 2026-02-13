7b2151d105835bcfa6c027b72d9082451303702a
Mattecl16 <camellini.matteo@iscmosuperiori.com>
2026-02-13 16:55:23 +0100

    viva la figa

--- lab2/python/main.py
+++ lab2/python/main.py
@@ -12,12 +12,23 @@ def main():
     ret = minimize(objective ,start,bounds=[(0,C) for b in range(N)], constraints={'type':'eq', 'fun':zerofun})
     #alpha = ret[’x’]
 
-def linear_kernel(x):
-    # the faster way to do the Xt * y where y=x is to do x * xT
-    return numpy.dot(x,x.T)
+def linear_kernel(x, y):
+    """ Returns the scalar product. """
+    return np.dot(x, y.T)
 
-def polynomial_kernel(x,p):
-    return math.pow(numpy.dot(x,x.T)+1,p)
+def polynomial_kernel(x, y, p=3):
+    """ Returns (x.y + 1)^p """
+    return (np.dot(x, y.T) + 1) ** p
+
+def rbf_kernel(x, y, sigma=0.5):
+    """ Returns exp(-||x-y||^2 / (2*sigma^2)) """
+    # Efficient pairwise squared distance
+    x_sq = np.sum(x**2, axis=1).reshape(-1, 1)
+    y_sq = np.sum(y**2, axis=1).reshape(1, -1)
+    dist_sq = x_sq + y_sq - 2 * np.dot(x, y.T)
+    # Clip small negatives due to precision
+    dist_sq = np.maximum(dist_sq, 0)
+    return np.exp(-dist_sq / (2 * sigma**2))
 
 def sigma(inputs):
     distance = []
@@ -26,8 +37,6 @@ def sigma(inputs):
             distance.append(numpy.linalg.norm(inputs[i]-inputs[j]))
     return statistics.median(distance)
     
-def radial_basis_function(x,y,sigma):
-    return math.exp(-numpy.linalg.norm(x-y)/(2*math.pow(sigma,2)))
 
 
 def objective():
